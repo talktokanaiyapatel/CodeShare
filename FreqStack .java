6b5322c42a7cb5e04626808f97369c80b5b87f4d
talktokanaiyapatel <talktokanaiyapatel@gmail.com>
2021-01-21 22:19:39 -0600

    Create FreqStack .java
    This implementation  uses Map and stack.

---
+++ FreqStack .java
@@ -0,0 +1,56 @@
+/**Que - Maximum Frequency Stack
+
+Solution
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].   **/
+
+
+class FreqStack {
+    //array for frequency
+    Map<Integer, Integer> freq;// item -> frequency
+    //store frequncy -> stack(all items of same frequency..)
+    Map<Integer, Stack<Integer>> group;
+    int maxfreq;
+
+    public FreqStack() {
+        freq = new HashMap();
+        group = new HashMap();
+        maxfreq = 0;
+    }
+
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        // points to current highest frequency.. since we do not have PQ sorting..
+        if (f > maxfreq)
+            maxfreq = f;
+
+        group.computeIfAbsent(f, z-> new Stack()).push(x);
+    }
+
+    public int pop() {
+        int x = group.get(maxfreq).pop();
+        freq.put(x, freq.get(x) - 1);
+        // if all elements in frequency empty, then reduce freq.	
+        if (group.get(maxfreq).size() == 0)
+            maxfreq--;
+        return x;
+    }
+}
